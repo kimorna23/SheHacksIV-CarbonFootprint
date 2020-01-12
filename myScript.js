caafec523d65e847727b88956ede87602691cf32
Xinrui Wang <lea.wangxr@gmail.com>
2020-01-12 02:23:27 -0500

    option visibility

--- myScript.js
+++ myScript.js
@@ -9,16 +9,22 @@ function hideAll(question){
 	document.getElementById(question + "1").style.visibility  = "hidden";
 	document.getElementById(question + "2").style.visibility  = "hidden";
 	document.getElementById(question + "3").style.visibility  = "hidden";
+}
+
+function hideSix(question){
+	document.getElementById(question + "1").style.visibility  = "hidden";
+	document.getElementById(question + "2").style.visibility  = "hidden";
+	document.getElementById(question + "3").style.visibility  = "hidden";
 	document.getElementById(question + "4").style.visibility  = "hidden";
 	document.getElementById(question + "5").style.visibility  = "hidden";
 	document.getElementById(question + "6").style.visibility  = "hidden";
-	questionsAnswered += 1;
 }
 
 function calculate(e, question, emissions, feedback){
 	console.log(e.srcElement.id);
 	var id = e.srcElement.id;
 	score = score + emissions;
+	if (question == "a2"){		hideSix(question);	}
 	hideAll(question);
 	document.getElementById(id).style.visibility  = "visible";
 	document.getElementById(id).disabled  = true;
