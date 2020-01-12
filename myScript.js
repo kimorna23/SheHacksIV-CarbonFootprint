f36bd9fd3aa89d14fe994fa8d6b165ec842ab08a
Maya Bielecki <maya.bielecki@gmail.com>
2020-01-11 23:17:57 -0500

    some changes to the js and fixed image issue

--- myScript.js
+++ myScript.js
@@ -1,37 +1,24 @@
 var score = 0;
+var totalfeedback = "";
 const numOfQuestions = 3;
 var questionsAnswered = 0;
+
 document.getElementById("score").innerHTML = score;
 
 function hideAll(question){
 	document.getElementById(question + "1").style.visibility  = "hidden";
 	document.getElementById(question + "2").style.visibility  = "hidden";
 	document.getElementById(question + "3").style.visibility  = "hidden";
+	document.getElementById(question + "4").style.visibility  = "hidden";
+	document.getElementById(question + "5").style.visibility  = "hidden";
+	document.getElementById(question + "6").style.visibility  = "hidden";
 	questionsAnswered += 1;
 }
 
-function low(e, question) {
+function calculate(e, question, emissions, feedback){
 	console.log(e.srcElement.id);
 	var id = e.srcElement.id;
-	score = score + 1;
-	hideAll(question);
-	document.getElementById(id).style.visibility  = "visible";
-	document.getElementById(id).disabled  = true;
-	document.getElementById("score").innerHTML = score;
-}
-
-function high(e, question) {
-	var id = e.srcElement.id;
-	score = score + 10;
-	hideAll(question);
-	document.getElementById(id).style.visibility  = "visible";
-	document.getElementById(id).disabled  = true;
-	document.getElementById("score").innerHTML = score;
-}
-
-function mid(e, question) {
-	score = score + 5;
-	var id = e.srcElement.id;
+	score = score + emissions;
 	hideAll(question);
 	document.getElementById(id).style.visibility  = "visible";
 	document.getElementById(id).disabled  = true;
